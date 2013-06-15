671276e5f39257ca1e64c98bfa271e6b27fcccab
Brian Chu <bc@brianchu.com>
2013-06-15 13:35:29 -0700

    Login methods outlined

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,8 +2,28 @@
 
 angular.module('standhubApp')
   .controller('Login', ['$scope', 'firebase', function ($scope) {
-			// authClient.login('<provider>');
-		// authClient.logout();
-
+    // authClient.login('<provider>');
+    // authClient.logout();
+    
+    $scope.facebookLogin = function() {
+      authClient.login('facebook', {rememberMe:true});
+    };
+    $scope.emailLogin = function(email, password) {
+      authClient.login('password', {
+        email: email,
+        password: password,
+        rememberMe:true
+      });
+    };
+    $scope.emailCreate = function(email,password) {
+      authClient.createUser(email, password, function(error, user) {
+        if (!error) {
+          console.log('User Id: ' + user.id + ', Email: ' + user.email);
+        }
+      });
+    };
+    $scope.logout = function() {
+      authClient.logout();
+    };
 
   }]);
