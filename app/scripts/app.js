67851e512f40a5a7bc89428a29d956889cfc8b38
gdi2290 <github@gdi2290.com>
2013-06-15 14:32:48 -0700

    add ui.bootstrap

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('standhubApp', [])
+angular.module('standhubApp', ['ui.bootstrap'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
