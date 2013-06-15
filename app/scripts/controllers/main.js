bd4ff7c9fdfced0215f6134653fdf2044e690e2d
Brian Chu <bc@brianchu.com>
2013-06-15 16:08:57 -0700

    Handle duplicate usernames

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,21 +5,32 @@ angular.module('standhubApp')
     $scope.fireUrl = 'https://standhubdev.firebaseio.com/';
     $scope.firebase = new Firebase($scope.fireUrl);
     $scope.user = {};
-    $scope.userRef;
+
+    $scope.allUsernames = {};
+    var listRef = new Firebase($scope.fireUrl+'users');
+    listRef.on('child_added', function(snapshot) {
+      var usrData = snapshot.val();
+      $scope.allUsernames[usrData.username] = true;
+    });
+
     $scope.authClient = new FirebaseAuthClient($scope.firebase, function(error, user) {
       if (error) {
         // an error occurred while attempting login
         console.log(error);
       } else if (user) {
         $scope.user = user;
+        if (!$scope.allUsernames[user.username]) {
+          var usersPushRef = listRef.push();
+          usersPushRef.set(user);
+        }
         console.log(user);
       } else {
         $scope.user = null;
       }
     });
-    // $scope.authTest = function() {
-    //   $scope.authClient.login('facebook', {rememberMe:true});
-    // };
+    $scope.authTest = function() {
+      $scope.authClient.login('facebook', {rememberMe:true});
+    };
 
 
 
