5e67156e75820ed1513719c02604b952a87259ca
Brian Chu <bc@brianchu.com>
2013-06-15 13:27:26 -0700

    Basic login outline

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('standhubApp')
+  .controller('Login', ['$scope', 'firebase', function ($scope) {
+			// authClient.login('<provider>');
+		// authClient.logout();
+
+
+  }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('standhubApp', ['firebase'])
-  .controller('MainCtrl', ['$scope', 'firebase', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', ['$scope', 'firebase', 'angularFire', function ($scope, angularFire) {
+    $scope.firebase = new Firebase("https://standhubdev.firebaseio.com");
+		$scope.authClient = new FirebaseAuthClient($scope.firebase, function(error, user) {
+		  if (error) {
+		    // an error occurred while attempting login
+		    console.log(error);
+		  } else if (user) {
+		    // user authenticated with Firebase
+		    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+		  } else {
+		    // user is logged out
+		  }
+		});
+
+
+
+
   }]);
