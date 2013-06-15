c52153ae73dce8df343811cc193660aafdb6a4a9
Brian Chu <bc@brianchu.com>
2013-06-15 13:12:42 -0700

    Added firebase

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('standhubApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+angular.module('standhubApp', ['firebase'])
+  .controller('MainCtrl', ['$scope', 'firebase', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
