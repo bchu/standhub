32498cc56552abf29deb3e6f15895eefb8a768f5
Brian Chu <bc@brianchu.com>
2013-06-15 14:29:35 -0700

    Added firebase url

--- app/scripts/controllers/help.js
+++ app/scripts/controllers/help.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('standhubApp')
-  .controller('HelpCtrl', ['$scope', 'firebase','angularFire', function ($scope, angularFire) {
-    
+  .controller('HelpCtrl', ['$scope', 'angularFire', function ($scope, angularFire) {
+    var url = $scope.fireUrl + 'helpReqs';
+    var promise = angularFire(url, $scope, 'helpReqs', []);
+
+    promise.then(function() {
+      // Or, attach a function to $scope that will let a directive in markup manipulate the model.
+      $scope.removeItem = function() {
+        $scope.items.splice($scope.toRemove, 1);
+        $scope.toRemove = null;
+      };
+    });
+
+
     $scope.submitRequest = function(tags,comment) {
-      
+
     };
 
   }]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,8 @@
 
 angular.module('standhubApp')
   .controller('MainCtrl', ['$scope', 'firebase', 'angularFire', function ($scope) {
-    $scope.firebase = new Firebase("https://standhubdev.firebaseio.com");
+    $scope.fireUrl = 'https://standhubdev.firebaseio.com/';
+    $scope.firebase = new Firebase($scope.fireUrl);
     $scope.authClient = new FirebaseAuthClient($scope.firebase, function(error, user) {
       if (error) {
         // an error occurred while attempting login
