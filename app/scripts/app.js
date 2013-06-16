435f89f826310bd5117853e1f0a1eda4d29e0c55
gdi2290 <github@gdi2290.com>
2013-06-16 01:20:39 -0700

    add momentjs and angular-momentjs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('standhubApp', ['firebase', 'ui.bootstrap','ui.select2'])
+angular.module('standhubApp', ['firebase', 'ui.bootstrap','ui.select2', 'angularMoment'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
