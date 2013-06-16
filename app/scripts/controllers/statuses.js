e8065c958c33d8d5bc262d6a9b74fd987d358778
Brian Chu <bc@brianchu.com>
2013-06-15 20:33:17 -0700

    Refactor user list

--- app/scripts/controllers/statuses.js
+++ app/scripts/controllers/statuses.js
@@ -2,19 +2,5 @@
 
 angular.module('standhubApp')
   .controller('StatusesCtrl', ['$scope', 'Data', function ($scope, Data) {
-    $scope.users = [];
-    Data.forEachUser(function(user) {
-      console.log(user);
-      $scope.users.push(user);
-      $scope.$apply();
-    });
-
-    // $scope.users = [{
-    //   firstname: 'gdi2290',
-    //   status: 'yolo',
-    // },
-    // {
-    //   firstname: 'gdi2290',
-    //   status: 'yolo',
-    // }];
+    $scope.users = Data.users;
   }]);
