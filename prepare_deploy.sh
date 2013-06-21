cb94e9ffbed7521c80f99d4e49a68373c8bbcc79
Brian Chu <bc@brianchu.com>
2013-06-21 16:13:04 -0700

    Updated deploy script

--- prepare_deploy.sh
+++ prepare_deploy.sh
@@ -4,6 +4,8 @@ cd gh_pages
 git init
 git config user.name "Brian Chu"
 git config user.email bc@brianchu.com
+git remote add origin https://github.com/bchu/standhub.git
+git remote set-url origin https://github.com/bchu/standhub.git
 git checkout -b gh-pages
 git pull origin gh-pages
 cd ..
