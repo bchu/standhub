0116b5096bccff3d6014cbbcb8e9c8a20eb1515c
gdi2290 <github@gdi2290.com>
2013-10-13 16:01:07 -0700

    add more configuration to app

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -11,6 +11,14 @@ app.config(['$routeProvider', function($routeProvider) {
     .otherwise({
       redirectTo: '/'
     });
-}]);
+}])
+.config(['$locationProvider', function($locationProvider) {
+  // $locationProvider.html5Mode(true).hashPrefix('!');;
+}])
+.run(['$rootScope', '$routeParams', '$log', function($rootScope, $routeParams, $log) {
+  $rootScope.$routeParams = $routeParams;
+  $rootScope.$log = $log;
+}])
+
 
 }(angular.module('standhub')));
