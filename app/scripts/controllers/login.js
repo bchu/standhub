ed2bdf7b9bc7f71c484f7f36d449a84882cf1993
gdi2290 <github@gdi2290.com>
2013-10-13 15:07:43 -0700

    refactor login controller to new module pattern

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,60 +1,67 @@
+;(function(app) {
 'use strict';
 
-angular.module('standhubApp')
-  .controller('LoginCtrl', ['$scope', 'Data', function ($scope, Data) {
+/* Login Controller */
+app.controller('LoginCtrl', ['$scope', 'Data', function($scope, Data) {
 
+  $scope.loggedIn = false;
+  $scope.$watchCollection(
+  function () {return Data.user;},
+  function (user) {
+
+    $scope.user = user;
+
+    if (user) {
+      var tags = [];
+      _(user.tags).each(function(tag) {
+        tags.push({id:tag,text:tag});
+      });
+      $scope.tags = tags;
+      $scope.loggedIn = true;
+    }
+
+  });
+
+  //options for tag field
+  $scope.tagOptions = {
+    tags: ["AngularJS", "JavaScript","Objective-C", "iOS","Unix","Pitch decks"],
+    tokenSeparators: [",",";"],
+    initSelection : function (element, callback) {
+      callback($scope.tags);
+    }
+  };
+
+  $scope.facebookLogin = function() {
+    Data.authClient.login('facebook', {rememberMe:true});
+    $scope.loggedIn = 'pending';
+  };
+
+  $scope.addTags = function(tags) {
+    if (!tags) { return; }
+    tags = _.pluck(tags,'text');
+    Data.userRef.child('tags').set(tags);
+  };
+
+  $scope.logout = function() {
+    Data.authClient.logout();
     $scope.loggedIn = false;
-    $scope.$watchCollection(
-    function () {return Data.user;},
-    function (user) {
-      $scope.user = user;
-      if (user) {
-        var tags = [];
-        _.each(user.tags, function(tag) {
-          tags.push({id:tag,text:tag});
-        });
-        $scope.tags = tags;
-        $scope.loggedIn = true;
-      }
-    });
+    $scope.tags = [];
+  };
 
-    //options for tag field
-    $scope.tagOptions = {
-      tags:["AngularJS", "JavaScript","Objective-C", "iOS","Unix","Pitch decks"],
-      tokenSeparators: [",",";"],
-      initSelection : function (element, callback) {
-        callback($scope.tags);
-      }
-    };
+  //ui customization
+  $scope.opts = {
+    backdropFade: true,
+    dialogFade:true
+  };
 
-    $scope.facebookLogin = function() {
-      Data.authClient.login('facebook', {rememberMe:true});
-      $scope.loggedIn = 'pending';
-    };
+  $scope.open = function () {
+    $scope.shouldBeOpen = true;
+  };
 
-    $scope.addTags = function(tags) {
-      if (!tags) {
-        return;
-      }
-      tags = _.pluck(tags,'text');
-      Data.userRef.child('tags').set(tags);
-    };
+  $scope.close = function (tags) {
+    $scope.shouldBeOpen = false;
+  };
 
-    $scope.logout = function() {
-      Data.authClient.logout();
-      $scope.loggedIn = false;
-      $scope.tags = [];
-    };
+}]);
 
-    //ui customization
-    $scope.opts = {
-      backdropFade: true,
-      dialogFade:true
-    };
-    $scope.open = function () {
-      $scope.shouldBeOpen = true;
-    };
-    $scope.close = function (tags) {
-      $scope.shouldBeOpen = false;
-    };
-  }]);
+}(angular.module('controllers')));
