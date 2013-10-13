0fcdef79a20c1fdb89ab5aac0d790603e251f4ba
gdi2290 <github@gdi2290.com>
2013-10-13 15:05:03 -0700

    refactor status controller to new module pattern

--- app/scripts/controllers/status.js
+++ app/scripts/controllers/status.js
@@ -1,27 +1,30 @@
+;(function(app) {
 'use strict';
 
-angular.module('standhubApp')
-  .controller('StatusCtrl', ['$scope', 'Data', function ($scope, Data) {
-    $scope.userStatus = '';
-    $scope.submitStatus = function(userStatus) {
-      if (Data.userRef) {
-        Data.userRef.child('status').set(userStatus);
-        Data.userRef.child('timestamp').set(new Date());
-      }
-      $scope.close();
-    };
+/* Status Controller */
+app.controller('StatusCtrl', ['$scope', 'Data', function ($scope, Data) {
+  $scope.userStatus = '';
+  $scope.submitStatus = function(userStatus) {
+    if (Data.userRef) {
+      Data.userRef.child('status').set(userStatus);
+      Data.userRef.child('timestamp').set(new Date());
+    }
+    $scope.close();
+  };
 
-    //ui customization:
-    $scope.opts = {
-      backdropFade: true,
-      dialogFade:true
-    };
-    $scope.open = function () {
-      $scope.shouldBeOpen = true;
-    };
-    $scope.close = function () {
-      if ($scope.shouldBeOpen) {
-        $scope.shouldBeOpen = false;
-      }
-    };
-  }]);
+  //ui customization:
+  $scope.opts = {
+    backdropFade: true,
+    dialogFade:true
+  };
+  $scope.open = function () {
+    $scope.shouldBeOpen = true;
+  };
+  $scope.close = function () {
+    if ($scope.shouldBeOpen) {
+      $scope.shouldBeOpen = false;
+    }
+  };
+}]);
+
+}(angular.module('controllers')));
