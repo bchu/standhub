94ac352d8b1bde6b585a444322afbf40496bb0ef
gdi2290 <github@gdi2290.com>
2013-10-13 15:09:52 -0700

    refactor alerts controller to new module pattern

--- app/scripts/controllers/alerts.js
+++ app/scripts/controllers/alerts.js
@@ -1,46 +1,54 @@
+;(function(app) {
 'use strict';
 
-angular.module('standhubApp')
-  .controller('AlertsCtrl', ['$scope', 'Data', '$timeout', function ($scope, Data,$timeout) {
-    $scope.$watch(
-      function() {return Data.requestsToYou;},
-      function(requestsToYou) {
-        $scope.requestsToYou = requestsToYou;
-    }, true);
-    $scope.$watch(
-      function() {return Data.requestsFromYou;},
-      function(requestsFromYou) {
-        $scope.requestsFromYou = requestsFromYou;
-    }, true);
+/* Alert Controller */
+app.controller('AlertsCtrl', ['$scope', 'Data', '$timeout', function($scope, Data,$timeout) {
 
-    // var callback = function() {
-    //   var d = Data;
-    //   console.log(d);
-    //   $timeout(callback, 3000);
-    // };
-    // $timeout(callback, 3000);
+  $scope.$watch(
+    function() {return Data.requestsToYou;},
+    function(requestsToYou) {
+      $scope.requestsToYou = requestsToYou;
+  }, true);
 
-    //need to update to show accepted status
-    $scope.accept = function(request) {
-      var url = Data.requestsUrl + request.refName + '/targets/';
-      var ref = (new Firebase(url)).child(Data.user.id);
-      ref.set('accept',function() {
-        // Data.refreshRequests();
-      });
+  $scope.$watch(
+    function() {return Data.requestsFromYou;},
+    function(requestsFromYou) {
+      $scope.requestsFromYou = requestsFromYou;
+  }, true);
 
-    };
-    $scope.decline = function(request) {
-      var url = Data.requestsUrl + request.refName + '/targets/';
-      var ref = (new Firebase(url)).child(Data.user.id);
-      ref.set('decline',function() {
-        // Data.refreshRequests();
-      });
-    };
-    $scope.cancel = function(request) {
-      var url = Data.requestsUrl + request.refName;
-      var ref = new Firebase(url);
-      ref.remove(function() {
-        // Data.refreshRequests();
-      });
-    };
-  }]);
+  // var callback = function() {
+  //   var d = Data;
+  //   console.log(d);
+  //   $timeout(callback, 3000);
+  // };
+  // $timeout(callback, 3000);
+
+  //need to update to show accepted status
+  $scope.accept = function(request) {
+    var url = Data.requestsUrl + request.refName + '/targets/';
+    var ref = (new Firebase(url)).child(Data.user.id);
+    ref.set('accept',function() {
+      // Data.refreshRequests();
+    });
+
+  };
+
+  $scope.decline = function(request) {
+    var url = Data.requestsUrl + request.refName + '/targets/';
+    var ref = (new Firebase(url)).child(Data.user.id);
+    ref.set('decline',function() {
+      // Data.refreshRequests();
+    });
+  };
+
+  $scope.cancel = function(request) {
+    var url = Data.requestsUrl + request.refName;
+    var ref = new Firebase(url);
+    ref.remove(function() {
+      // Data.refreshRequests();
+    });
+  };
+
+}]);
+
+}(angular.module('controllers')));
